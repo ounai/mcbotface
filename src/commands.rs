4b395b1323554e23fa19314c6226bb834ab9c429
Onni Rantanen <ounaifi@gmail.com>
2022-04-19 13:52:11 +0000

    Days until date command

--- src/commands.rs
+++ src/commands.rs
@@ -6,11 +6,16 @@ use teloxide::{
     types::User,
 };
 
+use chrono::prelude::*;
+
 #[derive(BotCommand, Clone)]
 #[command(rename = "lowercase", description = "Supported commands:")]
 enum Command {
     #[command(description = "send a hello message")]
     Hello,
+
+    #[command(description = "count days to a date")]
+    Tj,
 }
 
 trait GetSender {
@@ -39,6 +44,19 @@ fn get_hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+fn get_days_until(date: &str) -> String {
+    let now = Utc::now();
+
+    let then = DateTime::parse_from_rfc3339(&[date, "T00:00:00+00:00"].concat())
+        .expect("Invalid date string");
+
+    let duration = then.signed_duration_since(now).to_std().unwrap();
+
+    let days = (duration.as_secs() / 60 / 60 / 24) + 1;
+
+    format!("{} days until {}", days, date)
+}
+
 async fn on_command(
     bot: AutoSend<Bot>,
     message: Message,
@@ -46,7 +64,14 @@ async fn on_command(
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     match command {
         Command::Hello =>
-            bot.send_message(message.chat.id, get_hello(&message.get_sender())).await?,
+            bot.send_message(message.chat.id, get_hello(&message.get_sender()))
+                .reply_to_message_id(message.id)
+                .await?,
+
+        Command::Tj =>
+            bot.send_message(message.chat.id, get_days_until("2022-07-01"))
+                .reply_to_message_id(message.id)
+                .await?,
     };
 
     Ok(())
@@ -55,7 +80,7 @@ async fn on_command(
 pub async fn listen(bot: &'static AutoSend<Bot>) {
     println!("Initializing command listeners...");
 
-    let listener = teloxide::repls2::commands_repl(bot, on_command, Command::ty());
+    let listener = teloxide::repls2::commands_repl(bot.clone(), on_command, Command::ty());
 
     println!("Listening for commands...");
 
