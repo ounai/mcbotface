1424d9fe811654f08a13bb6b6bff9faaf0c4bd7c
Onni Rantanen <ounaifi@gmail.com>
2022-04-20 18:46:33 +0000

    Mark catmate message as sent

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -13,7 +13,7 @@ use teloxide::prelude2::*;
 
 use crate::catmate::CatMateMessage;
 
-static SLEEP_DURATION_SECONDS: u64 = 1;
+static SLEEP_DURATION_MS: u64 = 1_000;
 
 pub fn init(
     bot: &'static AutoSend<Bot>,
@@ -32,18 +32,19 @@ pub fn init(
         }));
 
         loop {
-            let mut locked_catmate_message = *catmate_message.lock().unwrap();
+            let mut locked_catmate_message = catmate_message.lock().unwrap();
 
-            tokio::spawn(async move {
-                if locked_catmate_message.not_sent_yet() {
-                    locked_catmate_message.send(bot).await;
-                    locked_catmate_message.mark_as_sent();
-                }
-            });
+            if locked_catmate_message.not_sent_yet() {
+                locked_catmate_message.mark_as_sent();
 
-            //panic!("Catmate done!"); // TODO
+                let cloned_catmate_message = locked_catmate_message.clone();
 
-            sleep(Duration::from_millis(SLEEP_DURATION_SECONDS * 1000));
+                tokio::spawn(async move { cloned_catmate_message.send(bot).await });
+
+                panic!("debug: catmate done"); // TODO
+            }
+
+            sleep(Duration::from_millis(SLEEP_DURATION_MS));
 
             cron.tick();
         }
